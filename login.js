f5a928f58a67e50aa35e7d81a6aaa2c9aa4d7697
Diego Hernandez <diegohxxiv@gmail.com>
2021-07-14 15:59:50 -0700

    Fixed Website Issues
    Fixed Website Issues

--- login.js
+++ login.js
@@ -1,15 +1,18 @@
-// alert('Email: admin#meetyou.com, password: user2021')
 // Login
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const loginBtn = document.getElementById('login-btn');
-alert('Email: admin@meetyou.com, Password: user2021')
+const answersMessage = document.getElementById('answers');
+const closeMessageBtn = document.getElementById('close-message-btn');
+closeMessageBtn.addEventListener('click', () => {
+    answersMessage.style.display = 'none';
+});
 const logIn = () => {
     const rightEmail = 'admin@meetyou.com';
     const rightPassword = 'user2021';
     if(email.value === rightEmail && password.value === rightPassword){
-        window.open('home.html');
         window.close();
+        window.open('home.html');
     } else {
         alert('Email or password is incorrect.');
         email.value = '';
